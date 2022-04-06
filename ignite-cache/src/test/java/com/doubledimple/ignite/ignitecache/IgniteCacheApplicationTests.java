d32a8e0dfb2b23ceb0876a2c63f8f01303b6a511
doubleDimple <lovele.cn@gmail.com>
2022-04-06 22:07:58 +0800

    commit

---
+++ ignite-cache/src/test/java/com/doubledimple/ignite/ignitecache/IgniteCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubledimple.ignite.ignitecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
